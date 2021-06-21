62fe434be4b2fc3d944a1f38f4e04b505a3038ae
seri0zha <ise200017@gmail.com>
2021-06-21 12:14:59 +0300

    Add title component import

--- src/components/Filters/Stops/index.tsx
+++ src/components/Filters/Stops/index.tsx
@@ -1,12 +1,16 @@
+import Title from "../Title";
 import StopsButton from "./StopsButton";
 import styles from "./styles.module.css";
 
 const Stops = () => {
   return (
-    <div className={styles.StopsWrapper}>
-      <StopsButton stopsCount={-1}/>
-      {Array.from(Array(4).keys()).map(num => <StopsButton stopsCount={num}/>)}
+    <>
+      <Title>КОЛИЧЕСТВО ПЕРЕСАДОК</Title>
+      <div className={styles.StopsWrapper}>
+        <StopsButton stopsCount={-1}/>
+        {Array.from(Array(4).keys()).map(num => <StopsButton stopsCount={num}/>)}
     </div>
+    </>
   );
 };
 
