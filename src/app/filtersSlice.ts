7d54c3e28a6dce49229c69fbedb150cc74a92036
seri0zha <ise200017@gmail.com>
2021-06-21 09:35:30 +0300

    Modified filter state

--- src/app/filtersSlice.ts
+++ src/app/filtersSlice.ts
@@ -3,10 +3,20 @@ import { currencyType } from "./types";
 
 interface FiltersState {
   currency: currencyType
+  exchangeRates: {
+    USD: number,
+    EUR: number,
+  },
+  stops: boolean[]
 }
 
 const initialState: FiltersState = {
-  currency: "RUB"
+  currency: "RUB",
+  exchangeRates: {
+    USD: 72.8,
+    EUR: 86.3
+  },
+  stops: new Array(4).fill(false)
 }
 
 const filtersSlice = createSlice({
@@ -14,10 +24,24 @@ const filtersSlice = createSlice({
   initialState,
   reducers: {
     switchCurrency(state, action: PayloadAction<currencyType>) {
-      state.currency = action.payload;
+      state.currency = action.payload; // EUR, RUB or USD
+    },
+    toggleAllStops(state) {
+      let containsOnlyOneValue = new Set(state.stops).size === 1;
+      if (containsOnlyOneValue) {
+        state.stops = state.stops.map(stop => !stop);
+      } else {
+        state.stops = new Array(4).fill(true);
+      }
+    },
+    toggleCurrentStop(state, action: PayloadAction<number>) {
+      state.stops[action.payload] = !state.stops[action.payload];
     }
   }
 })
 
 export default filtersSlice.reducer;
-export const { switchCurrency } = filtersSlice.actions;
+export const { 
+  switchCurrency, 
+  toggleAllStops, 
+  toggleCurrentStop } = filtersSlice.actions;
