03c71556636eb14b87becbd8d03c1289b3017646
seri0zha <ise200017@gmail.com>
2021-06-21 13:02:28 +0300

    Fix AllStops button

--- src/components/Filters/Stops/StopsButton/index.tsx
+++ src/components/Filters/Stops/StopsButton/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { toggleAllStops, toggleCurrentStop } from "../../../../app/filtersSlice";
+import { toggleCurrentStop } from "../../../../app/filtersSlice";
 import { useAppDispatch, useAppSelector } from "../../../../app/hooks";
 import styles from "./styles.module.css";
 
@@ -9,37 +9,21 @@ interface ButtonProps {
 
 const StopsButton: FC<ButtonProps> = (props) => {
   const dispatch = useAppDispatch();
-  const stops = useAppSelector(state => state.filters.stops);
-
-  let buttonText = "";
-  if (props.stopsCount === -1) {
-    buttonText = "Все";
-  } else {
-    buttonText = 
+  let checked = useAppSelector(state => state.filters.stops[props.stopsCount]);
+  let buttonText =
     props.stopsCount === 0 ? "Без пересадок" :
     props.stopsCount === 1 ? "1 пересадка" :
     props.stopsCount >= 2 ? `${props.stopsCount} пересадок` : "";
-  }
   
   const onButtonClick = (): void => {
-    if (props.stopsCount === -1) {
-      dispatch(toggleAllStops());
-    } else {
-      dispatch(toggleCurrentStop(props.stopsCount as number));
-    }
+    dispatch(toggleCurrentStop(props.stopsCount as number));
   };
-
-  const activeClassName = (): string => {
-    if (props.stopsCount !== -1)
-      return stops[props.stopsCount as number] ? styles.selected : "";
-    else 
-      return (new Set(stops).size === 1 && stops[0]) ? styles.selected : "";
-  }
 
   return (
     <button 
-      className={styles.button + " " + activeClassName()}
+      className={styles.button}
       onClick={onButtonClick}>
+      <input type="checkbox" checked={checked}/>
       {buttonText}
     </button>
   );
