a89e89ba32a79059bdb32edf1edf3bf60fedb1bc
seri0zha <ise200017@gmail.com>
2021-06-21 09:36:53 +0300

    Apply filters to tickets

--- src/components/Tickets/index.tsx
+++ src/components/Tickets/index.tsx
@@ -5,19 +5,25 @@ import { ITicket } from '../../app/types';
 import fetchTickets from '../../api/fetchTickets';
 import Ticket from '../Ticket';
 import styles from './styles.module.css';
+import applyFilters from '../../api/applyFilters';
 const Tickets = () => {
 
-  const tickets = useAppSelector(state => state.tickets.tickets);
-
+  // filter and sort tickets
+  const tickets = useAppSelector(
+    state => applyFilters(state.tickets.tickets)
+    .sort((a, b) => a.price - b.price));
+    
   const dispatch = useAppDispatch();
+
+  // get tickets from JSON on first render
   useEffect(() => {
     dispatch(setTickets(fetchTickets()));
   }, []);
 
   return (
     <div className={styles.TicketsWrapper}>
-      {tickets ? tickets.map((ticket: ITicket) => 
-        <Ticket ticket={ticket}/>) : <></>}
+      {tickets.map((ticket: ITicket) => 
+        <Ticket ticket={ticket}/>)}
     </div>
   );
 };
