de653da3cd311716cb590f7b9d1825cd7f51e5b3
seri0zha <ise200017@gmail.com>
2021-06-23 15:08:15 +0300

    Format stops number

--- src/components/Ticket/TimeSection/index.tsx
+++ src/components/Ticket/TimeSection/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { getFormatTime } from "../../../api/formatDateTime";
 import styles from "./styles.module.css";
 
 interface TimeSectionProps {
@@ -10,9 +11,13 @@ interface TimeSectionProps {
 const TimeSection: FC<TimeSectionProps> = (props) => {
   return (
     <div className={styles.TimeSectionWrapper}>
-      <div>{props.departureTime}</div>
-      <div>{props.stops}</div>
-      <div>{props.arrivalTime}</div>
+      <div>{getFormatTime(props.departureTime)}</div>
+      <div className={styles.Stops}>
+        {props.stops === 0 ? "Без пересадок" : 
+         props.stops === 1 ? "1 пересадка" : `${props.stops} пересадки`}
+         <hr/>
+      </div>
+      <div>{getFormatTime(props.arrivalTime)}</div>
     </div>
   )
 };
